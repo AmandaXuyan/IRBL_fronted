0f860926b6dcf5ce10d02d7064f33e9777ad32a2
张许妍 <181250187@smail.nju.edu.cn>
2022-03-15 22:41:35 +0800

    feat: 富文本编辑框

--- src/views/issue/components/issueModal.vue
+++ src/views/issue/components/issueModal.vue
@@ -1,21 +1,32 @@
 <template>
-    <div class="issue-md-editor">
-
+    <div class="issue-editor">
+        <tinymce
+                id="myedit"
+                ref="editor"
+                v-model="issueContent"
+                :disabled="disabled"
+                @onClick="onClick"
+        />
+<!--        <button @click="saveIssue()">清空内容</button>-->
+<!--        <button @click="disabled = true">禁用</button>-->
     </div>
 </template>
 
 <script>
     import {mapActions, mapGetters, mapMutations} from "vuex";
+    import tinymce from './tinymce'
 
 
     export default {
         name: "issueModal",
         components:{
-
+            tinymce,
         },
         data(){
             return{
-                text: '',
+                disabled: false,
+                issueContent: 'Welcome to Use Tinymce Editor',
+                isabled: false,
             };
         },
         computed:{
@@ -31,7 +42,21 @@
             ]),
             ...mapActions([
             ]),
-
+            // 内容
+            getContent () {
+                console.log('内容', this.issueContent)
+            },
+            // 鼠标单击的事件
+            onClick (e, editor) {
+                console.log('Element clicked')
+                console.log(e)
+                console.log(editor)
+            },
+            // 清空内容
+            clear () {
+                console.log(this.$refs.editor)
+                this.$refs.editor.clear()
+            },
 
         },
     }
